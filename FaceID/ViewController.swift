81bafd2c97206278700d2ac1b1e4f95b62556429
Dominika <dominikapiganova@mail.ru>
2022-01-31 10:54:29 +0200

    Experience with Face ID :)

--- FaceID/ViewController.swift
+++ FaceID/ViewController.swift
@@ -5,15 +5,47 @@
 //  Created by Dominika Poleshyck on 30.01.22.
 //
 
+import LocalAuthentication
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 200, height: 50))
+        view.addSubview(button)
+        button.center = view.center
+        button.setTitle("Authorize", for: .normal)
+        button.backgroundColor = .purple
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
     }
-
-
+    
+    @objc func didTapButton() {
+        let context = LAContext()
+        var error: NSError? = nil
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "Please authorize with touch ID!"
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { [weak self] success, error in
+                DispatchQueue.main.async {
+                    guard success, error == nil else {
+                        // failed
+                        let alert = UIAlertController(title: "Failed to Authenticate", message: "Please, try again.", preferredStyle: .alert)
+                        alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: nil))
+                        self?.present(alert, animated: true, completion: nil)
+                        return
+                    }
+                    //show other screen
+                    //success
+                    let vc = UIViewController()
+                    vc.title = "Welcome!"
+                    vc.view.backgroundColor = .systemYellow
+                    self?.present(UINavigationController(rootViewController: vc), animated: true, completion: nil) }
+            }
+        } else {
+            // can not use
+            let alert = UIAlertController(title: "Unavailable", message: "You can't use this feature", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: nil))
+            present(alert, animated: true, completion: nil)
+        }
+    }
 }
-
